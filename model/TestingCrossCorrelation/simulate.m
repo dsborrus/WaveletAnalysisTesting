2dedd68152a1faf277ee151085e6fc37165653d9
dsborrus - home pc <dsborrus@gmail.com>
2020-06-03 13:47:16 -0400

    added option to choose spike train

--- model/TestingCrossCorrelation/simulate.m
+++ model/TestingCrossCorrelation/simulate.m
@@ -1,21 +1,41 @@
-% run simulation and print output
+%% Testing cross coorelation with a toy model
+% This simulation aims to test out predictions about using cross
+% coorelation on two possibly related neuronal recordings
+
+% First the code generates fictive spike trains. Either poisson proccesses
+% or repetative regular spikes with jitter. The user can play with all the
+% parameters in the %% Parameters section
 clear; close all;
 
 %% Parameters
 
 P.tmax = 20;   % maximum simulation time in secconds
 P.dt   = 0.001; % time step
+
+P.traintoggle = 2; % the switch for spike timings, 
+                     % If 1, the spike trains will be regular spaced inputs 
+                     % with some jitter.
+                     % If 2, one spikes train will be a poisson process and
+                     % the other will be a copy, with some jitter
+                     % If 3, both trains will be different poissons
+
+% regular spikes settings                                           
+P.N    = 10;   % number of neurons
+P.p    = 1;    % period of spiking
+P.vp   = 2;    % STD of spiking
+
+% poisson proccess settings
+P.lambda = 0.2; % average timing between spikes in seconds 
+P.jit    = 0.1; % std to jitter spike timings
+
+P.buf  = 5;    % amount of buffer time before/after spiking
+
+%% Presim calculations and initializations
+
 P.t    = 0:P.dt:P.tmax-P.dt; % time array
 P.tct  = [0:P.dt:P.tmax-P.dt+(180*P.dt)]'; % time array for convoluted 
                                            % signal (it's longer due to 
                                            % the convolution).
-
-P.N    = 10;   % number of neurons
-P.p    = 1;    % period of spiking
-P.vp   = 0.2;    % STD of spiking
-P.buf  = 5;    % amount of buffer time before/after spiking
-
-%% Presim calculations and initializations
 
 P.nsteps  = P.tmax/P.dt;
 P.nspikes = P.tmax/P.p - (P.buf/P.p-1) - (P.buf/P.p);
@@ -29,8 +49,19 @@ end
 %% Blocks
 
 % generate both spike trains
-[sumt1,tra1] = GenSpikTrain(P);
-[sumt2,tra2] = GenSpikTrain(P);
+switch P.traintoggle
+    case 1
+    % if the user wants regularly spaced spikes
+    [sumt1,tra1] = GenSpikTrain(P);
+    [sumt2,tra2] = GenSpikTrain(P);
+    case 2
+    % if the user wants a poisson distanced spikes
+    [sumt1] = GenSpikPoisT(P);
+    [sumt2] = JitterTrain(sumt1,P);
+    case 3
+    [sumt1] = GenSpikPoisT(P);
+    [sumt2] = GenSpikPoisT(P);
+end
 
 % convule the spike trains an alpha function
 [ctrain1,fil,Out] = Convolut_custom(sumt1,P);
@@ -121,6 +152,43 @@ if dofig
    subplot(2,1,2); hold on; title('Summed output of all neurons')
    plot(P.dt:P.dt:length(sumtrains)*P.dt,sumtrains)
 end
+
+end
+
+function [train] = GenSpikPoisT(P)
+
+    % adjusted lambda, adjusted for the dt and index stuff
+    adj_lam = P.lambda/P.dt;
+
+    % initialize train
+    train = zeros(length(P.t),1);
+
+    for k = 1:P.tmax/P.dt; if rand<1/adj_lam; train(k) = 1; end; end
+    
+    % debugging figure
+    if 0
+        figure
+        plot(P.t,train)
+        % average distance between spikes = 
+        adbs = mean(diff(find(train)))*P.dt
+    end
+    
+end
+
+function [t2] = JitterTrain(t1,P)
+
+    x = find(t1);
+    r = round( rand(length(x),1) * ( P.jit/P.dt ) );
+    y = x+r;
+    
+    t2 = zeros(length(t1),1);
+    t2(y) = 1;
+    
+    if 1
+        figure
+        plot(P.t,t1); hold on;
+        plot(P.t,t2);
+    end
 
 end
 
